772c9b153f67438b2aad2a38e915b2844c1ae1fb
mp <mp@10.11.8.123>
2020-03-08 14:06:08 +0100

    step 2

--- chatbot/main.js
+++ chatbot/main.js
@@ -43,3 +43,25 @@ b(()=> {
   console.log("Test 1");
   console.log("Print C");
 });
+
+
+const  print1 = (result) => {
+  console.log("Result:");
+  console.log(result);
+  console.log("End of result");
+};
+
+
+const  print2 = (result) => {
+  console.log(result);
+  };
+  
+ 
+ const  sum2num = (a,b, decorator) => {
+  const rest = a+b;
+  decorator(rest);
+  };
+  
+  sum2num(1,1, print1);
+  sum2num(2,2, print1);
+  sum2num(3,3, res =>  console.log(res));
