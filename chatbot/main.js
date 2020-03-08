f4092692de13c50b6231de9852405997a5200613
mp <mp@Admin-MacBook-Air.local>
2020-03-08 13:39:32 +0100

    init step  of project

---
+++ chatbot/main.js
@@ -0,0 +1,17 @@
+
+
+
+function dds(input) {
+    console.log("CALL FUnction " + input);
+    return 2;
+}
+
+
+
+const result = function dds(input) {
+    console.log("CALL FUnction " + input);
+    return 2;
+};
+
+
+result("test");
