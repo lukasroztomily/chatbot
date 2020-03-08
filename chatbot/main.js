8f3311004d315d1a4ad7462772e42a35ac95bac6
mp <mp@10.11.8.123>
2020-03-08 13:56:58 +0100

    step 2

--- chatbot/main.js
+++ chatbot/main.js
@@ -19,3 +19,27 @@ result("test");
 const erew = arg => {
     console.log("CALL FUnction " + arg);
 };
+
+
+
+function a () {
+    console.log("Print A");
+};
+
+
+
+
+
+function b (callback) {
+    console.log("Print B");
+    callback();
+};
+
+
+b(a);
+
+
+b(()=> {
+  console.log("Test 1");
+  console.log("Print C");
+});
